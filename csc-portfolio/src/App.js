b9fa37a16d21c6ca3e358186f913cb4c425112ee
KeiferE <keifer.edelmayer@gmail.com>
2020-10-18 16:13:06 -0700

    reset of linting and react scripts bug

--- csc-portfolio/src/App.js
+++ csc-portfolio/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import headshot from './headshot.jpg';
+// import logo from './logo.svg';
+// import headshot from './headshot.jpg';
 import './App.css';
-import Sidebar from './components/sidebar';
-import './components/css/Sidebar.css';
+// import Sidebar from './components/sidebar';
+// import './components/css/Sidebar.css';
 
 /*function App() {
   return (
@@ -30,22 +30,10 @@ class App extends Component {
   render() {
     return (
       
-      <div id="App">
-       <div id="container-wrap">
-        <Sidebar />
-        <div id ="page-wrap">
-          <h1 className="App-name"> Keifer Edelmayer</h1>
-          <img src={headshot} className="App-headshot" />
-          <h1 className="App-projects">Projects</h1>
-          <h1 className="App-about">About</h1>
-          <p className="App-slogan">Hi, I'm Keifer. I like to create, solve problems, and go hiking</p>
-          <div className="App-longdash"></div>
-          <p className="App-title">Developer</p>
-          <p>This will be the contact comp</p>
-        </div>
-       </div>
+      <div>
+        <h1>Hello world</h1>
       </div>
-    );
+    )
   }
 }
 
