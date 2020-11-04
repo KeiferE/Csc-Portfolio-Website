fbc6c43bf1c7bf58160e9cd2c8003169c9db4251
KeiferE <keifer.edelmayer@gmail.com>
2020-11-03 18:31:04 -0800

    basic setup for desktop view. TODO: create mobile view for nav

--- csc-portfolio/src/components/FixedNav.jsx
+++ csc-portfolio/src/components/FixedNav.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
+import { Flex, Heading, Menu, MenuItem, Box } from "@chakra-ui/core";
 
 export default class FixedNav extends Component {
   render() {
     return (
-      <div>
-        <ul>home</ul>
-        <ul>projects</ul>
-        <ul>work</ul>
-        <ul>contact</ul>
-      </div>
+      <Flex
+        display={{ base: "none", md: "flex" }}
+        justifyContent="space-around"
+      >
+        <Flex>
+          <Heading>Home</Heading>
+        </Flex>
+        <Flex>
+          <Box m={10}>Projects</Box>
+          <Box m={10}>Work</Box>
+          <Box m={10}>Contact</Box>
+        </Flex>
+      </Flex>
     );
   }
 }
